cdfc542d4cc55a0f98fd182be3c6eadd683015cd
Azka Farhan <azkafarhanm@gmail.com>
2026-03-31 21:27:27 +0700

    feat: complete Day 5 with line ending fix

---
+++ 05_Day_Lists/day_5_lists.py
@@ -0,0 +1,5 @@
+my_list = ["Javascript", "Python", "Git"]
+my_list.append("Linux")
+print("Full Skill List:", my_list)
+print("Primary Skill:", my_list[0])
+print("Total number of skills:", len(my_list))
