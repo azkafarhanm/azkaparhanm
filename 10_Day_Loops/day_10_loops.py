758391d0dace3881c0b069a0c10185079a1fbd7a
azkafarhanm <azkafarhanm@gmail.com>
2026-04-07 10:26:41 +0700

    feat : Day 10 with practice

---
+++ 10_Day_Loops/day_10_loops.py
@@ -0,0 +1,25 @@
+﻿# --- Day 10: The Loop Master ---
+# 1 Using for loop with range
+print("Counting from 1 to 10:")
+for i in range(1, 11):
+    print(f"Number: {i}")
+
+# 2 Using fo loop with a List
+print("\nChecking the Student List: ")
+students = ["Budi", "azka", "orang", "lope"]
+
+for name in students:
+    print(f"Is {name} in class? Yes")
+
+# 3 Using While loop with CountDown
+print("\nClass start in:")
+timer = 3
+while timer > 0:
+    print(f"{timer}...")
+    timer -= 1
+print("Go!")
+
+
+
+
+     
