7a866249ac50cee46999c193298c0998a265084c
azkafarhanm <azkafarhanm@gmail.com>
2026-04-08 21:07:26 +0700

    feat: start Day 11 functions practice

---
+++ 11_Day_Functions/day_11_functions.py
@@ -0,0 +1,17 @@
+def greet_student():
+    print("Good morning everyone!")
+    print("please open your book")
+
+greet_student()
+
+def personal_greet(name):
+    print(f"Hello {name}, Good Morning!")
+
+personal_greet("azka")
+personal_greet("rizky")
+
+def add_number(number1, number2):
+    result = number1 + number2
+    print(result)
+
+add_number(20, 90)
