cb6cf72e54c308f53b02c6fa24a05c6fbd821af7
azkafarhanm <azkafarhanm@gmail.com>
2026-04-10 15:04:07 +0700

    feat: start day 12 modules practice

---
+++ Day_12_Modules/day_12_modules.py
@@ -0,0 +1,9 @@
+# Import the toolbox
+import os
+import math
+
+# Use a tool from the "os" module to see your current location
+print("Curren Folder Path:", os.getcwd())
+
+# Use a toolfrom the "math" module to find a square root
+print("Square root of 64 is:", math.sqrt(64))
