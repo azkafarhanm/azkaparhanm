eb42036473495b985371671a169e10f470c25b6f
azkafarhanm <azkafarhanm@gmail.com>
2026-04-11 16:54:13 +0700

    feat: mastered Day 13 List Comprehension

---
+++ Day_12_Modules/day_13_list_comprehension.py
@@ -0,0 +1,8 @@
+student = ["azka", "budi", "iki", "ilham"]
+name = [name.upper() for name in student ]
+print(name)
+
+
+numbers = [1, 2, 3, 4, 5, 6]
+evens = [i for i in numbers if i % 2 == 0]
+print(evens)
